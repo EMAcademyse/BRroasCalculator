e69f52ffab636d095f772ef0cfe2d50d2012f6ba
EMAcademyse <info@emacademy.se>
2025-05-02 00:03:10 +0300

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import BreakevenROASCalculator from './BreakevenROASCalculator'
+import './style.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BreakevenROASCalculator />
+  </React.StrictMode>,
+)
